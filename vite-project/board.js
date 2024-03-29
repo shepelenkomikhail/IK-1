1697f6dac96479fba72b86330334a4ec6927650e
Mykhailo Shepelenko <mikhael2005@gmail.com>
2024-03-29 16:44:43 +0100

    All items are arranged on a Board

--- vite-project/board.js
+++ vite-project/board.js
@@ -55,10 +55,103 @@ export class Board{
     }
 
     arrangeHidenComponents(){
+        let randomX, randomY;
+        let cell, freeCell;
+    
+        randomX = this.getRandomInt(5);
+        randomY = this.getRandomInt(5);
+        cell = this.board[randomX][randomY];
+    
+        freeCell = this.findCell(cell);
+        draw.drawItem1(freeCell[0], freeCell[1]);
+        this.board[freeCell[0]][freeCell[1]].type = "item1";
+        let item1 = this.board[freeCell[0]][freeCell[1]];
+        this.placeClueItem(item1, 1);
 
+        freeCell = this.findCell(cell);
+        draw.drawItem2(freeCell[0], freeCell[1]);
+        this.board[freeCell[0]][freeCell[1]].type = "item2";
+        let item2 = this.board[freeCell[0]][freeCell[1]];
+        this.placeClueItem(item2, 2);
+
+        freeCell = this.findCell(cell);
+        draw.drawItem3(freeCell[0], freeCell[1]);
+        this.board[freeCell[0]][freeCell[1]].type = "item3";
+        let item3 = this.board[freeCell[0]][freeCell[1]];
+        this.placeClueItem(item3, 3);
+    }
+
+    placeClueItem(item, n) {
+        const directions = ["left", "right", "up", "down"];
+        let emptyCells = [];
+    
+        this.board.forEach((row) => {
+            row.forEach((cell) => {
+                if (this.isEmptyCell(cell)) {
+                    emptyCells.push(cell);
+                }
+            });
+        });
+    
+        emptyCells.sort(() => Math.random() - 0.5);
+   
+        directions.forEach(direction => {
+            let [dx, dy] = [0, 0];
+            switch (direction) {
+                case "left":
+                    dy = 1;
+                    break;
+                case "right":
+                    dy = -1;
+                    break;
+                case "up":
+                    dx = 1;
+                    break;
+                case "down":        
+                    dx = -1;
+                    break;
+            }
+    
+            let found = false;
+            for (let i = 0; i < emptyCells.length; i++) {
+                const newX = item.x + dx * (i + 1);
+                const newY = item.y + dy * (i + 1);
+
+                if (newX >= 0 && newX < this.board.length && newY >= 0 && newY < this.board[0].length) {
+                    const nextCell = this.board[newX][newY];
+    
+                    if (this.isEmptyCell(nextCell)) {
+                        draw.drawClueItem(newX, newY, direction, n);
+                        nextCell.type = `clueItem${n}`;
+                        found = true;
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+    
+            if (found) {
+                emptyCells = emptyCells.filter(cell => {
+                    const diffX = Math.abs(cell.x - item.x);
+                    const diffY = Math.abs(cell.y - item.y);
+                    return (diffX !== 0 || diffY !== 0) && (diffX !== 1 || diffY !== 1);
+                });
+            }
+        });
     }
 
     getRandomInt(max) { return Math.floor(Math.random() * max);}
+
+    findCell(cell) { 
+        let randomX, randomY;
+        do {
+            randomX = this.getRandomInt(5);
+            randomY = this.getRandomInt(5);
+            cell = this.board[randomX][randomY];
+        } while (!this.isEmptyCell(cell));
+        return [randomX, randomY];
+    }
 
     isEmptyCell(cell){
         if (cell.type === "base") return true;
--- vite-project/draw.js
+++ vite-project/draw.js
@@ -24,5 +24,55 @@ export class Draw{
         oasis.src = "./assets/Oasis marker.png";
         cell.append(oasis);
     }
+
+    drawItem1(x,y){
+        let cell = document.querySelector(`.cell.row-${x}.col-${y}`);
+        cell.classList.add('bg-transparent');
+        let item1 = document.createElement('img');
+        item1.src = "./assets/Item 1.png";
+        cell.append(item1);
+    }
+
+    drawItem2(x,y){
+        let cell = document.querySelector(`.cell.row-${x}.col-${y}`);
+        cell.classList.add('bg-transparent');
+        let item2 = document.createElement('img');
+        item2.src = "./assets/Item 2.png";
+        cell.append(item2);
+    }
+
+    drawItem3(x,y){
+        let cell = document.querySelector(`.cell.row-${x}.col-${y}`);
+        cell.classList.add('bg-transparent');
+        let item3 = document.createElement('img');
+        item3.src = "./assets/Item 3.png";
+        cell.append(item3);
+    }
+
+    drawClueItem(x, y, direction, n){
+        let cell = document.querySelector(`.cell.row-${x}.col-${y}`);
+        console.log(cell);
+        console.log(x, y, direction, n);
+        cell.classList.add('bg-transparent');
+        let itemClue = document.createElement('img');
+        switch(direction){
+            case "left":
+                itemClue.src = `./assets/Item ${n} - clue_LEFT.png`;
+                console.log(itemClue.src);
+                break;
+            case "right":
+                itemClue.src = `./assets/Item ${n} - clue_RIGHT.png`;
+                break;
+            case "down":
+                itemClue.src = `./assets/Item ${n} - clue_DOWN.png`;
+                break;
+            case "up":
+                itemClue.src = `./assets/Item ${n} - clue_UP.png`;
+                break;
+            default:
+                break;
+        }
+        cell.append(itemClue);
+    }
 }
 
